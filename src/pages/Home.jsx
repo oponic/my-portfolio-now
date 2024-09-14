e29eb5a95cefb349de6d3e0e4e538bcd5c5df9ed
luq-mne <luqman.yazrie@outlook.com>
2024-09-15 00:46:42 +0800

    final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,10 +25,14 @@ export default function Home() {
     return (
         <div className="w-full lg:w-5/12 items-center mx-auto space-y-8">
             <Card rounded={true} flat={false}>
-                <p class="pb-16 text-2xl bg-gradient-to-r from-[rgb(76,194,255)] to-[rgb(214,76,255)] text-transparent bg-clip-text">
-                    A 15 year old student with big ambitions (execution is still a work-in-progress).
-                    Born and raised in Malaysia, by a tech entpreneur dad and computer science mom.
+                <p class="pb-16 text-3xl bg-gradient-to-r from-[rgb(76,194,255)] to-[rgb(214,76,255)] text-transparent bg-clip-text">
+                    A Malaysian high school student that knows the sky is not the limit, the specialist in generalising.
                 </p>
+
+                <h1 class="pb-8 text-xl">
+                    Howdy, the name's Luqman - also goes by Luq. Currently based in Kuala Lumpur, Malaysia.
+                    The renaissance man? Oh, it just means I do a bit of everything.
+                </h1>
 
                 <div class="flex space-x-4 font-mono-[rgb(25,25,25] pb-8 text-sm text-[rgb(214,76,255)]">
                       <button onclick={(interests)} class={`rounded-md p-2 ${show() === "interests" ? "bg-[rgb(40,40,40)] text-[rgb(76,194,255)]" : "bg-[rgb(20,20,20)]"}`}>interests</button>
@@ -44,10 +48,15 @@ export default function Home() {
                             data analysis, computer science and geopolitics.
                         </p>
 
-                        <p>
+                        <p className="pb-2">
                             I love researching strategies, corporations, products and individuals that
                             gained my admiration and interest to further understand how and why they got
                             their success - in hoping to learn from their success and failures.
+                        </p>
+
+                        <p>
+                            Other than these things, I also enjoy architecture, arts and music.
+                            Am known as a creative person who talks excessively and is a bit of a perfectionist.
                         </p>
                     </div>
                 )}
@@ -66,13 +75,31 @@ export default function Home() {
                     </div>
                 )}
 
+                {show() === "coding" && (
+                    <div>
+                        <p className="pb-2">
+                            All the cool things I think deserves a mention is under the Projects section.
+                        </p>
+
+                        <p className="pb-2">
+                            My preferred languages are JavaScript, Go, and Python. I also use x86 assembly
+                            for funsies, alongside C. JS framework of choice? Solidjs.
+                        </p>
+
+                        <p>
+                            I started actively coding in late July of 2023, and have gotten pretty good and
+                            used to most things. Would not admit that I am a pro yet, but I am optimistic
+                            that I can do great things with these newfound skills.
+                        </p>
+                    </div>
+                )}
+
             </Card>
 
             <Card rounded={true} flat={false}>
                 <h1 class="text-2xl pb-2 font-mono text-[rgb(214,76,255)]">projects</h1>
-
                 <p class="pb-8">
-                    Some projects that I want to highlight on
+                    All the things that I have worked on that deserves a feature!
                 </p>
 
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -97,7 +124,15 @@ export default function Home() {
             </Card>
 
             <Card rounded={true} flat={false}>
-                <h1>Hello</h1>
+                <h1 class="text-2xl pb-2 font-mono text-[rgb(214,76,255)]">contact</h1>
+                <p class="pb-8">
+                    This section exists only because I have little to no idea what else to put lmao.
+                </p>
+
+                <p>
+                    Have some idea on what other content to add in this portfolio? Uhh give me a message!
+                    I check the inboxes of my public social media accounts every now and then.
+                </p>
             </Card>
         </div>
     )
