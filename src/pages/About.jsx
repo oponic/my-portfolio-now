afe717fba411654b083d2f4a1917b5e080545617
luq-mne <luqman.yazrie@outlook.com>
2024-09-17 15:26:42 +0800

    started work on interests

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,3 @@
-import openIcon from "../assets/icons/open.svg"
-
 import Top from "../components/Top"
 import Card from "../components/Card"
 
@@ -52,11 +50,34 @@ export default function Home() {
                     Interests
                 </h1>
 
-                <Card>
-                    <p>
-                        Loads of things.
-                    </p>
-                </Card>
+                <p>Loads of things!</p>
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-4 md:text-center">
+                    <div class="p-2 rounded-md text-xl outline outline-2 outline-[rgb(20,20,20)]">
+                        <p>Data Analysis</p>
+                    </div>
+
+                    <div class="p-2 rounded-md text-xl outline outline-2 outline-[rgb(20,20,20)]">
+                        <p>Economics</p>
+                    </div>
+
+                    <div class="p-2 rounded-md text-xl outline outline-2 outline-[rgb(20,20,20)]">
+                        <p>Semiconductors</p>
+                    </div>
+
+                    <div class="p-2 rounded-md text-xl outline outline-2 outline-[rgb(20,20,20)]">
+                        <p class="text-[rgb(249,231,159)]">
+                            Programming
+                        </p>
+                    </div>
+
+                    <div class="p-2 rounded-md text-xl outline outline-2 outline-[rgb(20,20,20)]">
+                        <p>Finance</p>
+                    </div>
+
+                    <div class="p-2 rounded-md text-xl outline outline-2 outline-[rgb(20,20,20)]">
+                        <p>Startup Development</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
