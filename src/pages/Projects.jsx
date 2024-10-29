01b797d848205bab40b1f94837d3279aa6927eea
opo <169742991+oponic@users.noreply.github.com>
2024-10-29 18:39:21 -0500

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,49 +14,17 @@ export default function Projects() {
             <div class="pb-32 space-y-4">
                 <Card rounded={true}>
                     <p>
-                        Here are some of my projects that I think
-                        deserves to be shown at.
+                        I don't update websites or anything often, so I refuse to state projects I may scrap here.
                     </p>
                 </Card>
 
                 <p class="text-sm">
-                    All of my other publicly-available projects can be found on my 
+                    All of my publicly-available projects can be found on my 
                     <A href="https://github.com/theluqmn/repositories" target="_blank" class="text-[rgb(93,173,226)]">
                         GitHub profile
                     </A>.
                 </p>
             </div>
-
-            <div class="pb-32 grid grid-cols-1 md:grid-cols-2 gap-4">
-                <Card rounded={true}>
-                    <div class="space-y-4">
-                        <h1 class="text-xl">
-                            theluqmn.github.io
-                        </h1>
-
-                        <img src={PortfolioPicture} className="w-full h-26 rounded-md outline outline-[rgb(25,25,25)]"/>
-
-                        <p>
-                            My personal portfolio website made using Solidjs,
-                            and hosted with GitHub Pages.
-                        </p>
-                    </div>
-                </Card>
-
-                <Card rounded={true}>
-                    <div class="space-y-4">
-                        <h1 class="text-xl">
-                            F-35C
-                        </h1>
-
-                        <img src={F35CPicture} className="w-full h-26 rounded-md outline outline-[rgb(25,25,25)]"/>
-
-                        <p>
-                        Generic, multi-role open-source Discord bot
-                        designed for you to self-host and use as a template.
-                        </p>
-                    </div>
-                </Card>
             </div>
         </div>
     )
