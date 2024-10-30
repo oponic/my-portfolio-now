08c6ccd8ccf99d73fa53799202a98cddfd7e3f07
luq-mne <luqman.yazrie@outlook.com>
2024-10-31 06:37:14 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,21 @@ import instagramIcon from "./assets/icons/instagram.svg"
 import xIcon from "./assets/icons/x.svg"
 
 export default function App(props) {
-    return(
+    return (
         <div class="w-full">
-            <header
-            class="p-4 text-xl flex items-center justify-between fixed z-10 bg-[rgba(25,25,25,0.5)] w-full backdrop-blur-sm outline outline-2 outline-[rgb(20,20,20)]">
+            <header class="p-4 text-xl flex items-center justify-between fixed z-10 bg-[rgba(25,25,25,0.5)] w-full backdrop-blur-sm outline outline-2 outline-[rgb(20,20,20)]">
                 <div class="w-full md:w-5/12 mx-auto flex items-center justify-between">
-                    <div class="flex items-center gap-4 hover:scale-105 duration-500">
-                        <img src={pfp} class="w-9 h-9 rounded-full object-cover outline outline-[rgb(93,173,226)]" alt="luqman profile picture"/>
-                        <h1>jack</h1>
+                        <div class="flex items-center gap-4 hover:scale-105 duration-500">
+                            <img src={pfp} class="w-9 h-9 rounded-full object-cover outline outline-[rgb(93,173,226)]" alt="luqman profile picture"/>
+                            <h1>jack</h1>
+                        </div>
+                        
+                        <div>
+                            <A href="https://github.com/oponic" target="_blank">
+                                <img src={githubIcon} class="w-6 h-6 invert hover:scale-110 duration-300"/>
+                            </A>
+                        </div>
                     </div>
-                        <A href="https://github.com/oponic" target="_blank">
-                            <img src={githubIcon} class="w-6 h-6 invert hover:scale-110 duration-300"/>
-                        </A>
-                    </div>
-                </div>
             </header>
 
             <div class="p-4 w-full pt-28">
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -25,7 +25,6 @@ export default function Projects() {
                     </A>.
                 </p>
             </div>
-            </div>
         </div>
     )
 }
